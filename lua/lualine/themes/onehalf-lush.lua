00bdade3690828e21a5db6ac129b49104bada52f
Magnus Åsrud <magnusasrud92@gmail.com>
2022-12-02 19:37:07 +0100

    Theme/lualine (#2)
    Added lualine theme

---
+++ lua/lualine/themes/onehalf-lush.lua
@@ -0,0 +1,67 @@
+local colors = {}
+
+-- The colors are taken from https://github.com/sonph/onehalf.
+if vim.o.background == 'light' then
+  colors.black   = "#383a42"
+  colors.red     = "#e45649"
+  colors.green   = "#50a14f"
+  colors.yellow  = "#c18401"
+  colors.blue    = "#0184bc"
+  colors.purple  = "#a626a4"
+  colors.white   = "#fafafa"
+  colors.inactive_bg = "#f0f0f0"
+
+  colors.a_fg = colors.white
+  colors.b_fg = colors.white
+  colors.b_bg = colors.black
+  colors.c_fg = colors.black
+  colors.c_bg = colors.white
+else
+  colors.black   = "#282c34"
+  colors.red     = "#e06c75"
+  colors.green   = "#98c379"
+  colors.yellow  = "#e5c07b"
+  colors.blue    = "#61afef"
+  colors.purple  = "#c678dd"
+  colors.white   = "#dcdfe4"
+  colors.inactive_bg = "#313640"
+
+  colors.a_fg = colors.black
+  colors.b_fg = colors.black
+  colors.b_bg = colors.white
+  colors.c_fg = colors.white
+  colors.c_bg = colors.black
+end
+
+return {
+  normal = {
+    a = { bg = colors.blue,   fg = colors.a_fg, gui = 'bold' },
+    b = { bg = colors.b_bg,   fg = colors.b_fg },
+    c = { bg = colors.c_bg,   fg = colors.c_fg }
+  },
+  insert = {
+    a = { bg = colors.green,  fg = colors.a_fg, gui = 'bold' },
+    b = { bg = colors.b_bg,   fg = colors.b_fg },
+    c = { bg = colors.c_bg,   fg = colors.c_fg }
+  },
+  visual = {
+    a = { bg = colors.purple, fg = colors.a_fg, gui = 'bold' },
+    b = { bg = colors.b_bg,   fg = colors.b_fg },
+    c = { bg = colors.c_bg,   fg = colors.c_fg}
+  },
+  replace = {
+    a = { bg = colors.red,    fg = colors.a_fg, gui = 'bold' },
+    b = { bg = colors.b_bg,   fg = colors.b_fg },
+    c = { bg = colors.c_bg,   fg = colors.c_fg }
+  },
+  command = {
+    a = { bg = colors.yellow, fg = colors.a_fg, gui = 'bold' },
+    b = { bg = colors.b_bg,   fg = colors.b_fg },
+    c = { bg = colors.c_bg,   fg = colors.c_fg }
+  },
+  inactive = {
+    a = { bg = colors.inactive_bg, fg = colors.black, gui = 'bold' },
+    b = { bg = colors.inactive_bg, fg = colors.black },
+    c = { bg = colors.inactive_bg, fg = colors.black },
+  }
+}
