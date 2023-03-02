c43e4fd27482ed1c13946dc1b7146421a6d90f81
Magnus Åsrud <magnusasrud92@gmail.com>
2023-03-02 22:18:09 +0100

    Split light and dark theme

---
+++ colors/onehalf-lush-dark.lua
@@ -0,0 +1,1 @@
+require("onehalf-lush").setup("dark")
--- lua/onehalf-lush/init.lua
+++ lua/onehalf-lush/init.lua
@@ -2,46 +2,30 @@
 -- Lua port of https://github.com/sonph/onehalf
 -- By https://github.com/sonph/onehalf
 -- The code is heavily based on http://github.com/mofiqul work.
-local onehalf_lush = {}
+local M = {}
 local theme = require('onehalf-lush.theme')
 
-onehalf_lush.setup = function(user_opts)
-  local defaults = {
-    transparent = false,
-    italic_comments = true,
-    color_overrides = {},
-    group_overrides = {},
-    disable_nvimtree_bg = true,
-  }
-
-  -- Override default settings with user settings.
-  local opts = vim.tbl_extend('force', defaults, user_opts)
-
-  -- setting transparent to true removes the default background
-  if opts.transparent then
-    opts.color_overrides.bg = 'NONE'
-  end
-
+-- @param string style
+function M.setup(style)
   vim.cmd('hi clear')
   if vim.fn.exists('syntax_on') then
     vim.cmd('syntax reset')
   end
 
   vim.opt.termguicolors = true
-  vim.g.colors_name = 'onehalf-lush'
+  vim.g.colors_name = "onehalf-lush"
 
-  theme.set_highlights(opts)
+  theme.setup(style)
   theme.link_highlight()
-
-  for group, val in pairs(opts['group_overrides']) do
-    vim.api.nvim_set_hl(0, group, val)
-  end
 end
 
-onehalf_lush.change_style = function(style)
+-- @param string style
+function M.change_style(style)
   vim.opt.background = style
+  theme.setup(style)
+  theme.link_highlight()
   print('onehalf-lush style: ', style)
   vim.cmd([[colorscheme onehalf-lush]])
 end
 
-return onehalf_lush
+return M
